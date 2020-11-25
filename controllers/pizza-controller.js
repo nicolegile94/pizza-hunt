19f1dea424ac1da8c6b94b15972a4a484ab88f9c
Nicole Gile <nicolegile94@gmail.com>
2020-11-25 13:29:48 -0600

    added comment functionality

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,27 +4,39 @@ const pizzaController = {
 
     getAllPizza(req, res) {
         Pizza.find({})
-        .then(dbPizzaData => res.json(dbPizzaData))
-        .catch(err => {
+          .populate({
+            path: 'comments',
+            select: '-__v'
+          })
+          .select('-__v')
+          .sort({ _id: -1 })
+          .then(dbPizzaData => res.json(dbPizzaData))
+          .catch(err => {
             console.log(err);
             res.status(400).json(err);
-        });
-    },
+          });
+      },
 
     getPizzaById({ params }, res) {
-        Pizza.findOne({_id: params.id})
+        Pizza.findOne({ _id: params.id })
+        .populate({
+          path: 'comments',
+          select: '-__v'
+        })
+        .select('-__v')
         .then(dbPizzaData => {
-            if (!dbPizzaData) {
-                res.status(404).json({message: 'No pizza found with this id!'});
-                return;
-            }
-            res.json(dbPizzaData);
+          if (!dbPizzaData) {
+            res.status(404).json({ message: 'No pizza found with this id!' });
+            return;
+          }
+          res.json(dbPizzaData);
         })
         .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
+          console.log(err);
+          res.status(400).json(err);
         });
     },
+    
     // createPizza
     createPizza({ body }, res) {
         Pizza.create(body)
