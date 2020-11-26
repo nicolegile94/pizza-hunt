adb80660c4629e2be41e7abc45fb3e44c4bc0c2d
Nicole Gile <nicolegile94@gmail.com>
2020-11-26 14:39:43 -0600

    add replies

--- routes/api/comment-routess.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const {addComment, removeComment} = require('../../controllers/comment-controller');
-
-router.route('/:pizzaId').post(addComment);
-
-router.route('/:pizzaId/:commentId').delete(removeComment);
-
-
-module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const commentRoutes = require('./comment-routess');
+const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
 
 router.use('/comments', commentRoutes);
