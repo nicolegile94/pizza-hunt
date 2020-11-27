509fc3b3d4791775e5ca5b849f8b4ded8179e1c8
Nicole Gile <nicolegile94@gmail.com>
2020-11-26 19:24:56 -0600

    validators

--- models/Comment.js
+++ models/Comment.js
@@ -1,3 +1,4 @@
+const { request } = require('express');
 const {Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
@@ -9,10 +10,13 @@ const ReplySchema = new Schema(
             default: () => new Types.ObjectId()
         },
         replyBody: {
-            type: String
+            type: String,
+            required: true,
+            trim: true
         },
         writtenBy: {
-            type: String
+            type: String,
+            required: true
         },
         createdAt: {
             type: Date,
